21c45234e85c4404cd6a78b4e7411f86c9362e4d
Chris <chris.haack@me.com>
2013-12-18 11:48:43 -0500

    refactor methods

--- dog.rb
+++ dog.rb
@@ -1,84 +1,117 @@
 require 'mysql2'
 require 'debugger'
 
+ 
 class Dog
+  attr_accessor :name, :color, :id 
 
-  DB = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "dogs")
-
-  attr_accessor :name, :color
-
-  def initialize(name, color)
+  def initialize(name, color, id=nil)
     @name = name
     @color = color
+    @id = id
+  end
+
+  @@db = Mysql2::Client.new(
+    :host => "localhost", 
+    :username => "root", 
+    :database => "dogs")
+
+  def db
+    @@db
   end
 
   def self.db
-    DB
+    @@db
   end
 
-  def db
-    DB
+  def insert
+    db.query("insert into dogs(name, color)
+      VALUES('#{self.name}', '#{self.color}')
+      ")
+      self.id = db.last_id
   end
 
-  def self.find_dog_by_id(id)
-    self.db.query("
-      SELECT * 
+
+  def self.find_by_id(id)
+    temp_array = []
+    temp =self.db.query("SELECT *
       FROM dogs
-      WHERE id = #{id}
-      ")
-      my_dogs = []
-      temp = Dog.new(result["name"])
+      WHERE id = #{id}")
+    temp.each do |item|
+      temp_array << Dog.new(item["name"], item["color"], item["id"])
+    end
+    temp_array
   end
 
-  def self.find_dog_by_name(name)
-    self.db.query("
-      SELECT * 
+  def self.find_by_name(name)
+    self.db.query("SELECT *
       FROM dogs
-      WHERE name = '#{name}'
-      ")
+      WHERE name = '#{name}' ").first
   end
 
-  def save_dog
-    results = self.db.query("INSERT INTO dogs(name, color)
-      VALUES('#{self.name}', '#{self.color}')"
-      )
+  def self.find_by_color(color)
+    self.db.query("SELECT *
+      FROM dogs
+      WHERE color = '#{color}' ").first
   end
 
-  def change_dog(name, color, id)
-    self.db.query("UPDATE dogs
-      SET name = '#{name}', color = '#{color}'
-      WHERE id = #{id}
-      ")
+  def mark_saved!
+    self.id = self.db.last_id if self.db.last_id > 0
   end
 
-  def self.new_from_db row
-    dog = Dog.new(row["name"], row["color"])
-    dog.id = row["id"]
-    dog  
-  end 
+  #save for later, 
+  def update
+    # if
+    # mark_saved! 
+      db.query("
+        UPDATE dogs
+        SET name = '#{name}', color ='#{color}'
+        WHERE id = #{self.id}
+        ")
+     # else
+     #  puts "sorry, nothing to update, the puppy doesn't exist!"
+     # end
+  end
 
-  def mark_as_saved!
-      self.id = self.db.last_id if self.db.last_id > 0
+  def save
+    if self.id > 0
+      self.update
+    else
+      self.insert
+    end
+    #if it has been saved, update
+    #if it hasn't been saved, insert
+  end
+
+  def self.delete(id)
+    #if marked_saved! == true
+      self.db.query("
+        DELETE FROM dogs
+        WHERE id = #{id}")
+    # else
+    #   puts "sorry, no record there."
+    # end
   end
 
 end
 
-dog = Dog.new("tucker", "blue")
-dog.save_dog
-puts Dog.find_dog(1).first
-dog.change_dog("Marco", "red", 1)
-#{"id"=>1, "name"=>"tucker", "color"=>"blue"}
+
+# pup = Dog.find_by_name("fido") 
+# puts pup
+#puts pup.methods-Object.methods
+#Dog.find_by_att(1)
+ 
+#dog = Dog.find(10)
 #debugger
 #puts 'hi'
-
+ 
   # color, name, id
   # db
-  # find_by_att
-  # find
-  # insert
+
+
   # update
   # delete/destroy
-
+ 
   # refactorings?
   # new_from_db?
   # saved?
@@ -89,4 +122,3 @@ dog.change_dog("Marco", "red", 1)
   # inspect
   # reload
   # attributes
-
